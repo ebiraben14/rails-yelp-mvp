5449ef9eab776c8308042a1e3b1cabad7d33965f
Emmanuel BIRABEN <ebiraben14@hotmail.com>
2021-05-12 17:19:11 +0200

    exos2 o

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+entrcote = Restaurant.create!(name: 'Entrecote', address:'Neuilly' , category: 'french' , phone_number:'062240' )
+veggie = Restaurant.create!(name: 'Veggie', address:'Boulogne' , category: 'italian' , phone_number:'063350')
+lamoelle = Restaurant.create!(name: 'La moelle', address:'Paris' , category: 'belgian' , phone_number:'064460')
+lenemroyal= Restaurant.create!(name: 'Le Nem Royal', address:'bordeaux' , category: 'chinese' , phone_number:'065580')
+kobe= Restaurant.create!(name: 'Kobe', address:'Tokyo' , category: 'japanese' , phone_number:'068870')
+
+
+
